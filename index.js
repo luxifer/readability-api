5e6089f7e1e46762bf556de414f7ec9bdb082a12
Florent Viel <luxifer666@gmail.com>
2017-05-22 11:05:26 +0200

    refacto with promise

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const url = require('url');
 const { Readability, JSDOMParser } = require('readability/index');
+const contentType = require('content-type');
 
 const server = http.createServer((req, res) => {
     const query = url.parse(req.url, true).query;
@@ -11,11 +12,8 @@ const server = http.createServer((req, res) => {
         res.end();
     }
 
-    http.get(query.url, (response) => {
-        let content = '';
-
-        response.on('data', (chunk) => { content += chunk; });
-        response.on('end', () => {
+    getUrl(query.url)
+        .then((content) => {
             let document = new JSDOMParser().parse(content);
             const loc = url.parse(query.url, true);
             const uri = {
@@ -29,8 +27,31 @@ const server = http.createServer((req, res) => {
             res.setHeader('Content-Type', 'application/json');
             res.write(JSON.stringify(article));
             res.end();
+        }, (err) => {
+            console.error(err);
+            res.statusCode = 400;
+            res.statusMessage = 'Bad request';
+            res.write(JSON.stringify({
+                'error': err.message
+            }));
+            res.end();
+        });
+});
+
+function getUrl(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, (res) => {
+            let obj = contentType.parse(res.headers['content-type']);
+            if (obj.type != 'text/html') {
+                reject(new Error('Accept only text/html'));
+            }
+
+            let content = '';
+            res.on('data', (chunk) => { content += chunk; });
+            res.on('end', () => { resolve(content); });
+            res.on('err', (err) => { reject(err); })
         });
     });
-});
+}
 
 server.listen(4567);
